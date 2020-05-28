84b249afc9247ac5805b52c6e5f7c2c829082119
aleVeD <aldmove@gmail.com>
2020-05-28 17:55:40 -0400

    adding owners list and update controller. closes #14

--- pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -15,10 +15,17 @@ public class DataLoader implements CommandLineRunner {
   private final OwnerService ownerService;
   private final VetService vetService;
 
-  public DataLoader() {
+  public DataLoader(OwnerService ownerService, VetService vetService) {
+    this.ownerService = ownerService;
+    this.vetService = vetService;
+  }
+
+  /* public DataLoader() {
     ownerService = new OwnerServiceMap();
     vetService = new VetServiceMap();
   }
+
+  */
 
   @Override
   public void run(String... args) throws Exception {
