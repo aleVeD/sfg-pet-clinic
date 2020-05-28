33c2a50b6c758e7e814253fb9dff097ff1cc137b
aleVeD <aldmove@gmail.com>
2020-05-28 15:38:27 -0400

    adding bootstrap for data. closes #12

---
+++ pet-clinic-web/src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -0,0 +1,39 @@
+package guru.springframework.bootstrap;
+
+import guru.springframework.model.Owner;
+import guru.springframework.model.Vet;
+import guru.springframework.services.OwnerService;
+import guru.springframework.services.VetService;
+import guru.springframework.services.map.OwnerServiceMap;
+import guru.springframework.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+  private final OwnerService ownerService;
+  private final VetService vetService;
+
+  public DataLoader() {
+    ownerService = new OwnerServiceMap();
+    vetService = new VetServiceMap();
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    Owner owner1 = new Owner();
+    owner1.setId(1L);
+    owner1.setFirstName("Pedro");
+    owner1.setLastName("Perez");
+    ownerService.save(owner1);
+    System.out.println("Load owners");
+    Vet vet1 = new Vet();
+    vet1.setId(1L);
+    vet1.setFirstName("Claudio");
+    vet1.setLastName("Fernandez");
+    vetService.save(vet1);
+    System.out.println("loaded vets ...");
+
+  }
+}
