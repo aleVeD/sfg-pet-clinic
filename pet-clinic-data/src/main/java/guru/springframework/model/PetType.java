96386e856191ebea1a1a88318657d8f9fc706514
aleVeD <aldmove@gmail.com>
2020-06-09 12:46:34 -0400

    Update owners to jpa entities. closes #34

--- pet-clinic-data/src/main/java/guru/springframework/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/PetType.java
@@ -1,7 +1,13 @@
 package guru.springframework.model;
 
-public class PetType extends BaseEntity{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table("pet_types")
+public class PetType extends BaseEntity{
+  @Column(name = "name")
   private String name;
 
   public String getName() {
