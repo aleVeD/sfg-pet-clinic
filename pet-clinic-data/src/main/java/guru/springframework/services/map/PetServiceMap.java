88e9dbd0d016872aa19f88e94ae4fa731d3c4b2f
aleVeD <aldmove@gmail.com>
2020-06-11 15:21:16 -0400

    Updates profiles for JPA.closes #47

--- pet-clinic-data/src/main/java/guru/springframework/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/services/map/PetServiceMap.java
@@ -3,10 +3,12 @@ package guru.springframework.services.map;
 import guru.springframework.model.Pet;
 import guru.springframework.services.CrudService;
 import guru.springframework.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 @Service
+@Profile({"default", "map"})
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
   @Override
   public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/guru/springframework/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/services/map/PetTypeMapService.java
@@ -2,10 +2,12 @@ package guru.springframework.services.map;
 
 import guru.springframework.model.PetType;
 import guru.springframework.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
   @Override
   public Set<PetType> findAll() {
