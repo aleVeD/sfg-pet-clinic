3a335751e7d7b0464e0b6bd608b9f74f93e1ac50
aleVeD <aldmove@gmail.com>
2020-06-17 14:38:20 -0400

    Upgrade to juni5.closes #4

---
+++ pet-clinic-web/src/test/java/guru/springframework/SfgPetClinicApplicationTest.java
@@ -0,0 +1,21 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(Extension.class)
+@SpringBootTest
+class SfgPetClinicApplicationTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void main() {
+  }
+}
